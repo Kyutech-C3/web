debe554cf399f547f4860089dc846d7b47028127
pikake <maha.jasminum@gmail.com>
2021-09-18 08:28:58 +0900

    [clean] #1 フォントをカラーとサイズのみ指定に変更、コメントアウトの整理

--- components/TopAboutCommunity.vue
+++ components/TopAboutCommunity.vue
@@ -3,11 +3,12 @@
         <div class = "community-wrapper">
                 <div class = "box text-contents">
                     <h1>コミュニティ</h1>
-                    <p>九工大のサークル、<span>C3</span>です。</p>
-                    <p><span>Unity</span>を使ってゲームを作ったり、<span>Web</span>の開発、<span>Blender</span>で<span>3D</span>モデリングなど、コンピュータを使って色々な創作活動を行っているサークルです。</p>
+                    <p>九工大のサークル、C3です。</p>
+                    <p>Unityを使ってゲームを作ったり、Webの開発、Blenderで3Dモデリングなど、コンピュータを使って色々な創作活動を行っているサークルです。</p>
                 </div>
 
             <div class = "box community-icons">
+                <!-- アイコンはお好きな順番で -->
                 <!-- メディアアートコミュニティのアイコン -->
                 <img class = "icon art" src = "https://3.bp.blogspot.com/-To8tA3yiE0k/VJF_QawmPdI/AAAAAAAAp0w/oihka8c4k90/s800/animalface_usagi.pngS"/>
 
@@ -27,48 +28,42 @@
 
 
 <script>
-// JavaScript使わん
 export default {
-
 }
 </script>
 
 
 <style lang = "scss" scoped>
-/* しも先輩にきく SCSSわからん*/
-
+    
+    /* ページ全体 */
     .community-wrapper {
         width: 100%;
-    
         display: flex;
-        flex-wrap: wrap; 
+        flex-wrap: wrap;
         padding: 30px;
 
         /* チェック用  */
         /* background-color: lightgreen; */
     }
-
+    
+    
+    /* 文章全体 */
     .text-contents {
-        width: 60%;
+        width: 50%;
         height: 10%;
-        margin-right: 2%;
+        margin-right: 5%;
         margin-bottom: 5%;  
+        margin-left:5%;
 
         /* チェック用 */
         /* background-color: skyblue; */
     }
 
-    p {
-        text-align: left;
-        font: normal normal normal $font-size-contents-description/45px Yu Gothic;
-        letter-spacing: 0px;
-        color: $base-font-color;
-    }
-
+    
+    /* 小見出し */
     h1 {
         text-align: center;
-        font: normal normal medium $font-size-contents-title/160px Yu Gothic;
-        letter-spacing: 0px;
+        font-size: $font-size-contents-title;
         color: $base-font-color;
         width: 70%;
         height: 10%;
@@ -76,14 +71,17 @@ export default {
         /* チェック用 */
         /* background-color: orange; */
     }
-
-    span {
+    
+    
+    /* 説明文章 */
+    p {
         text-align: left;
-        font: normal normal normal 18px/45px Inter;
-        letter-spacing: 0px;
+        font-size: $font-size-contents-description;
         color: $base-font-color;
     }
 
+    
+    /* アイコン全体 */
     .community-icons {
         width: 32%;
         height: 10%;
@@ -91,7 +89,6 @@ export default {
         margin-right: 3%;
         margin-bottom: 3%;
         padding-left: 3%;
-        /* display: flex; */
 
         /* アイコンを常に2＊2で並べるバージョン */
         /* dislay: grid; */
@@ -101,31 +98,35 @@ export default {
         /* background-color: tomato; */
     }
 
+    
+    /* アイコン1個 */
     .icon {
+        /* アイコンのサイズ */
         width: 80px;
         height: 80px;
 
-        /* 背景の円 */
+        /* アイコンの背景の円 */
         border-radius: 50%;
-        background: $base-font-color;
+        background-color: $base-font-color;
     }
 
+    
+    /* 画面小さくしたら縦並びになる仕様 */
     @media not all and (min-width: 600px) {
         .box {
             width: 100%;
         }
+        
         h1 {
             width: 100%;
         }
+        
         .text-contents {
             padding-right:5%;
-
         }
+        
         .community-icons {
-            /* width: 100%; */
             padding-left: 16%;
         }
-
-
     }
 </style>
