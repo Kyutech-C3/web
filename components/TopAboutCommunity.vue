d6db640f2b012ea512bfe0d677e190232716296b
pikake <maha.jasminum@gmail.com>
2021-09-26 13:19:58 +0900

    [change] #1 スマホ・タブレット版の画面の仕様追加

--- components/TopAboutCommunity.vue
+++ components/TopAboutCommunity.vue
@@ -2,25 +2,36 @@
     <div>
         <div class = "community-wrapper">
                 <div class = "box text-contents">
-                    <h1>コミュニティ</h1>
+                    <div class ="title-box">
+                    <p class="title">コミュニティ</p>
+                    </div>
+                    <div class ="description">
                     <p>九工大のサークル、C3です。</p>
                     <p>Unityを使ってゲームを作ったり、Webの開発、Blenderで3Dモデリングなど、コンピュータを使って色々な創作活動を行っているサークルです。</p>
+                    </div>
                 </div>
 
             <div class = "box community-icons">
-                <!-- アイコンはお好きな順番で -->
-                <!-- メディアアートコミュニティのアイコン -->
-                <img class = "icon art" src = "https://3.bp.blogspot.com/-To8tA3yiE0k/VJF_QawmPdI/AAAAAAAAp0w/oihka8c4k90/s800/animalface_usagi.pngS"/>
-
-                <!--ゲームコミュニティのアイコン -->
-                <img class = "icon game" src = "https://1.bp.blogspot.com/-ZqRV1i42ELM/VJF_J7IvQjI/AAAAAAAApzk/GCpLXcqU6WE/s800/animalface_neko.png"/> 
-                    
-                <!-- ハックコミュニティのアイコン -->
-                <img class = "icon hack" src = "https://2.bp.blogspot.com/-fhkRCjjEO98/VJF_LkOt_bI/AAAAAAAApzs/jYqrTFF6XA4/s800/animalface_niwatori.png"/>
-
-                <!-- CGコミュニティのアイコン -->
-                <img class = "icon cg" src = "https://4.bp.blogspot.com/-co9Gfae-aWE/XNE-4L7WvLI/AAAAAAABStA/kX0r_XmPRcgt8x69YxtLM3rZ_53H4Y-MgCLcBGAs/s800/food_niku_buta_ro-su.png">
-                
+                <div id="icon_set_1" class="icon_set">
+                    <div class="icon_box">
+                        <!-- メディアアートコミュニティのアイコン -->
+                        <img class = "icon art" src = "https://3.bp.blogspot.com/-To8tA3yiE0k/VJF_QawmPdI/AAAAAAAAp0w/oihka8c4k90/s800/animalface_usagi.pngS"/>
+                    </div>
+                    <div class="icon_box">
+                        <!--ゲームコミュニティのアイコン -->
+                        <img class = "icon game" src = "https://1.bp.blogspot.com/-ZqRV1i42ELM/VJF_J7IvQjI/AAAAAAAApzk/GCpLXcqU6WE/s800/animalface_neko.png"/> 
+                    </div>
+                </div>
+                <div id="icon_set_2" class="icon_set">  
+                    <div class="icon_box">
+                        <!-- ハックコミュニティのアイコン -->
+                        <img class = "icon hack" src = "https://2.bp.blogspot.com/-fhkRCjjEO98/VJF_LkOt_bI/AAAAAAAApzs/jYqrTFF6XA4/s800/animalface_niwatori.png"/>
+                    </div>
+                    <div class="icon_box">
+                        <!-- CGコミュニティのアイコン -->
+                        <img class = "icon cg" src = "https://4.bp.blogspot.com/-co9Gfae-aWE/XNE-4L7WvLI/AAAAAAABStA/kX0r_XmPRcgt8x69YxtLM3rZ_53H4Y-MgCLcBGAs/s800/food_niku_buta_ro-su.png">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -39,21 +50,18 @@ export default {
     .community-wrapper {
         width: 100%;
         display: flex;
+        align-items: center;
         flex-wrap: wrap;
-        padding: 30px;
 
         /* チェック用  */
         /* background-color: lightgreen; */
+
     }
     
     
     /* 文章全体 */
     .text-contents {
-        width: 50%;
-        height: 10%;
-        margin-right: 5%;
-        margin-bottom: 5%;  
-        margin-left:5%;
+        width: 60%;
 
         /* チェック用 */
         /* background-color: skyblue; */
@@ -61,12 +69,10 @@ export default {
 
     
     /* 小見出し */
-    h1 {
-        text-align: center;
+    .title {
+        text-align: left;
         font-size: $font-size-contents-title;
         color: $base-font-color;
-        width: 70%;
-        height: 10%;
         
         /* チェック用 */
         /* background-color: orange; */
@@ -83,50 +89,87 @@ export default {
     
     /* アイコン全体 */
     .community-icons {
-        width: 32%;
-        height: 10%;
-        margin-top: 6%;
-        margin-right: 3%;
-        margin-bottom: 3%;
-        padding-left: 3%;
-
-        /* アイコンを常に2＊2で並べるバージョン */
-        /* dislay: grid; */
-        /* grid-template-columns: 2fr 2fr; */
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        width: 320px;
+        height: 320px;
+        margin-left: 5%;
+        margin-right: 5%;
 
         /* チェック用 */
         /* background-color: tomato; */
     }
 
-    
-    /* アイコン1個 */
-    .icon {
-        /* アイコンのサイズ */
-        width: 80px;
-        height: 80px;
+    .icon_set {
+        display: flex;
+        justify-content: space-between;
+        width: 320px;
+        height: 160px;
+    }
 
-        /* アイコンの背景の円 */
+    #icon_set_1 {
+        display: flex;
+        align-items: flex-start;
+    }
+
+    #icon_set_2 {
+        display: flex;
+        align-items: flex-end;
+    }
+
+    .icon_box {
+        width: 100px;
+        height: 100px;
+        position: relative;
+
         border-radius: 50%;
         background-color: $base-font-color;
+        text-align: center;
+        
+    }
+    
+    /* 各アイコン */
+    .icon {
+        width: 40px;
+        height: 40px;
+        vertical-align: middle;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 
     
     /* 画面小さくしたら縦並びになる仕様 */
-    @media not all and (min-width: 600px) {
-        .box {
-            width: 100%;
-        }
-        
-        h1 {
-            width: 100%;
-        }
-        
+    @media not all and (min-width: 1400px) {
         .text-contents {
-            padding-right:5%;
+            width: 100%;
+            margin-left: 124px;
+            margin-right: 124px;
         }
-        
-        .community-icons {
-            padding-left: 16%;
+
+
+       .community-icons {
+           position: absolute;
+           left: 0;
+           right: 0;
+           margin: auto;
+       }
+       .community-wrapper {
+            position: relative;
+            margin: auto;
         }
+
+        .title {
+            text-align: center;
+            padding-bottom: 200px;
+            width: 100%;
+        }
+        .description {
+            padding-top: 200px;
+        }
+
+      
     }
 </style>
