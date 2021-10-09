60580670ae8862e04f206b5f688d3717b1d2cd72
Kizuku <kotani.kizuku@gmail.com>
2021-10-10 02:08:36 +0900

    [change] 横幅の修正

--- components/TopAboutC3.vue
+++ components/TopAboutC3.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="area">
-        <div class="icon">C3</div>
+        <img src="../c3_logo.png" alt="C3アイコン" class="icon">
             <div class="top">
                 C3について
             </div>
@@ -8,11 +8,11 @@
                 九工大のサークル、C3です。<br>
                 Unityを使ってゲームを作ったり、Webの開発、Blenderで3Dモデリングなど、コンピュータを使って色々な創作活動を行っているサークルです。
             </div>
-            <a class="link" href="about">
+            <nuxt-link to="/about" class="link">
                 <div class="frame">
                     詳しく見る
                 </div>
-            </a>
+            </nuxt-link>
     </div>
 </template>
 
@@ -24,22 +24,14 @@ flex-wrap: wrap;
 height: 500px;
 }
 .icon{
-margin: 0 0 0 120px;
+margin: 0 0 0 6.25%;
 width: 400px;
-height: 400px;
-line-height: 400px;
-border: solid #555555;
 border-radius: 50%;
-background: #555555;
-color: $white;
-font-size: 200px;
-font-family: "Inter";
-text-align: center;
 }
 
 .top{
-padding: 0 0 0 100px;
-width: 100%;
+padding: 0 0 0 5.2%;
+width: 536px;
 height: 129px;
 line-height: 160px;
 max-width: $max-width;
@@ -50,18 +42,19 @@ font-style: Medium;
 font-size: $font-size-contents-title;
 }
 .sentence{
-padding: 60px 0 0 108px;
-width: 1014px;
-height: 178px;
+padding: 60px 0 0 5.6%;
+width: 51.2%;
 line-height: 45px;
 text-align: left;
 max-width: $max-width;
+max-height: 200px;
 color: $gray;
 font-family: "Inter","游ゴシック";
 font-style: Regular;
 font-size: $font-size-contents-description;
 }
-.link{margin: 0 0 0 495px;
+.link{
+margin: 28px 0 0 25.7%;
 width: 240px;
 height: 60px;
 line-height: 39px;
@@ -93,13 +86,14 @@ background-color: $gray;
 
 @media screen and (max-width: 1000px){
 .area{
-height: 800px;
+height: auto;
 justify-content: center;
 }
 
 .top{
 padding: 0 0;
 text-align: center;
+width: 100%;
 height: 92px;
 line-height: 115px;
 font-size: $font-size-contents-title;
