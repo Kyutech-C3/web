606b99e5cc18049debd8d99f42760569f8ef0065
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-01-14 22:14:35 +0900

    [fix] #16 community-linkのデータ連携の修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <top-top :news="news" />
-    <top-about-c-3 :c3-introduction="c3Introduction" />
-    <top-about-community :about-community="aboutCommunity" />
-    <top-community-link :each-community="eachCommunity" />
-    <top-blog :blog="blog" />
+    <top-about-c-3 :c3-introduction="c3Introduction" class="component" />
+    <top-about-community :about-community="aboutCommunity" class="component" />
+    <top-community-link
+      v-for="(community, idx) in eachCommunity"
+      :id="idx"
+      :key="idx"
+      :each-community="eachCommunity"
+      :community="community"
+      class="component"
+    />
+    <top-blog :blog="blog" class="component" />
   </div>
 </template>
 
@@ -44,11 +51,22 @@ export default {
         }),
       ])
         .then(([c3Introduction, aboutCommunity, eachCommunity, news, blog]) => {
+          const communities = []
+          for (let i = 0; i < eachCommunity.items.length; i++) {
+            communities.push({
+              id: eachCommunity.items[i].sys.id,
+              field: {
+                name: eachCommunity.items[i].fields.name,
+                about: eachCommunity.items[i].fields.about,
+                image: eachCommunity.items[i].fields.image.fields.file.url,
+              },
+            })
+          }
           return {
             c3Introduction:
               c3Introduction.items[0].fields.summaryOfIntroduction,
             aboutCommunity: aboutCommunity.items[0].fields.about,
-            eachCommunity: eachCommunity.items,
+            eachCommunity: communities,
             news: news.items,
             blog: blog.items,
           }
