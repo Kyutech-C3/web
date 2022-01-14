01c00a7fbca87ee23f1ed606f987d154ff33c1cf
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2022-01-14 15:55:19 +0000

    Deploying to gh-pages from  @ 8dfd4f2044a18e57f208f9a73252139eb17ca82b 🚀

--- _nuxt/static/1642174765/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fweb\u002F_nuxt\u002Fstatic\u002F1642174765",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fweb\u002F",assetsPath:"\u002Fweb\u002F_nuxt\u002F",cdnURL:a}}}}(null));
