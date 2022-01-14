545e8b9906d8db30f2c9413a47e178fd357eea05
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2022-01-14 17:55:21 +0000

    Deploying to gh-pages from  @ 1c4bd3e3fb8fe9d51e4c9da8ad46c7702af4271f 🚀

--- _nuxt/de674cb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{396:function(n,e,d){"use strict";d.r(e);var l=d(28),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
