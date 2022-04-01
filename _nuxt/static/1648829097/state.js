e2480cf651d47e02827693f3d3398cbb0db5826e
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2022-04-01 16:05:01 +0000

    Deploying to gh-pages from  @ c54f9a08974532aa9475906aa482c4b8067bcbb0 🚀

---
+++ _nuxt/static/1648829097/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fweb\u002F_nuxt\u002Fstatic\u002F1648829097",layout:"default",error:a,state:{breadcrumbs:{breadcrumbs:[{url:b,text:"Composite Computer Club"}]}},serverRendered:true,routePath:b,config:{_app:{basePath:"\u002Fweb\u002F",assetsPath:"\u002Fweb\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
