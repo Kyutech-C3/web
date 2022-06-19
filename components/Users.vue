c059ddb222a2ef8e6899a72c454271c92a882d0d
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-06-19 23:54:13 +0900

    [add] #70 Usersコンポーネントの追加

---
+++ components/Users.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="users">
+    <user
+      v-for="(user, index) in users"
+      :id="user.sys.id"
+      :key="index"
+      :name="user.fields.name"
+      :icon="user.fields.icon.fields.file.url"
+      :show-name="true"
+      class="user"
+    />
+  </div>
+</template>
+
+<script>
+import User from '@/components/User.vue'
+export default {
+  components: {
+    User,
+  },
+  props: {
+    users: {
+      type: Array,
+      required: true,
+      default() {
+        return []
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.users {
+  display: flex;
+  width: 60%;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+}
+.user {
+  margin: 0 5px;
+}
+</style>
