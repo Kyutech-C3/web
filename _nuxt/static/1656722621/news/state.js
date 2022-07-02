b0faf380f460b2e99547b23dd5312fae2baa9e1f
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2022-07-02 00:43:44 +0000

    Deploying to gh-pages from  @ 6ad9c302e045a541bb278182f9e8be772c34b31d 🚀

---
+++ _nuxt/static/1656722621/news/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656722621",layout:"default",error:a,state:{breadcrumbs:{breadcrumbs:[{url:b,text:"ホーム"},{url:c,text:"お知らせ一覧"}]}},serverRendered:true,routePath:c,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F","\u002Fnews"));
